8111b9ba872f1e09976c05d2b1d2eeadca743882
Muhammed Ömer Faruk Selvi <84385239+mofthedev@users.noreply.github.com>
2025-08-09 15:58:13 +0300

    Add files via upload

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -15,30 +15,28 @@ class Index extends Controller
         // var_dump(Request::getPath());
         // var_dump(App::getAbsoluteProjectPath());
         // var_dump(App::getProjectPath());
-        $this->response->addContent("Hello! This is Index. ".Index::class);
-        $this->response->addContent("<br>Func args: ".implode(", ", $args));
-        $this->response->addContent("<br>Args: ".implode(", ", Request::getArgs()));
-        $this->response->addContent("<br>Args Assoc: ".print_r(Request::getArgsAssoc(), true));
-        $this->response->addContent("<br>Path: ".Request::getPath());
-        $this->response->addContent("<br>Path Segs: ".implode(", ", Request::getPathSegments()) );
-
-
+        $this->response->addContent("Hello! This is Index. " . Index::class);
+        $this->response->addContent("<br>Func args: " . implode(", ", $args));
+        $this->response->addContent("<br>Args: " . implode(", ", Request::getArgs()));
+        $this->response->addContent("<br>Args Assoc: " . print_r(Request::getArgsAssoc(), true));
+        $this->response->addContent("<br>Path: " . Request::getPath());
+        $this->response->addContent("<br>Path Segs: " . implode(", ", Request::getPathSegments()));
     }
 
     public function test2()
     {
-        $this->response->addContent("Hello! This is test2. ".Index::class);
-        $this->response->addContent("<br>Args: ".implode(", ", Request::getArgs()));
-        $this->response->addContent("<br>Path: ".Request::getPath());
+        $this->response->addContent("Hello! This is test2. " . Index::class);
+        $this->response->addContent("<br>Args: " . implode(", ", Request::getArgs()));
+        $this->response->addContent("<br>Path: " . Request::getPath());
     }
 
-    public function test($a1, ?int $a2=null, ?string $a3)
+    public function test($a1, ?int $a2 = null, ?string $a3)
     {
-        Session::set("deneme",123);
+        Session::set("deneme", 123);
         var_dump(Request::getArgsAssoc());
         var_dump(Request::getArgs());
         $this->response->addContent("Test this!");
-        $this->response->addContent("---".Session::get("denem")."---");
+        $this->response->addContent("---" . Session::get("denem") . "---");
         var_dump($a2);
     }
 
@@ -53,12 +51,13 @@ class Index extends Controller
     public function bulma()
     {
         Bulma::appendHead(
-            \HTML::css(App::getURIRoot()."/Public/custom.css")
+            \HTML::css(App::getURIRoot() . "/Public/custom.css")
         );
 
         $left_content = Bulma::Box("This is a box on the left side.", ["has-background-success-light", "has-text-weight-bold", "has-text-danger-dark"]);
 
-        $main_content = Bulma::Box(Bulma::Box("This is a box in another box. <b>This sentence is escaped.</b>", ["has-background-info", "has-text-weight-bold", "has-text-success-dark"]),
+        $main_content = Bulma::Box(
+            Bulma::Box("This is a box in another box. <b>This sentence is escaped.</b>", ["has-background-info", "has-text-weight-bold", "has-text-success-dark"]),
             ["has-background-link-light"]
         );
 
@@ -74,15 +73,28 @@ class Index extends Controller
                 Bulma::Container(
                     Bulma::Cols(
                         \View::concat(
-                            Bulma::Col($col1, [\BulmaClass::Is3]),
-                            Bulma::Col($col2, [\BulmaClass::Is6]),
-                            Bulma::Col($col3, [\BulmaClass::Is3])
+                            Bulma::Col($col1, [\BulmaClass::IS_3]),
+                            Bulma::Col($col2, [\BulmaClass::IS_6]),
+                            Bulma::Col($col3, [\BulmaClass::IS_3])
                         )
-                    ) ,
-                    [\BulmaClass::IsFluid]
+                    ),
+                    [\BulmaClass::IS_FLUID]
                 )
             ),
             "Bulma Test in Mortingen Framework"
         );
+    }
+
+    public function db()
+    {
+        $Q = "\DB\Query";
+
+        //$q = $Q::select("*")->from(\DB\Identifier::{"MyTable"}())->where(\DB\Identifier::{"Username"}(), "=", new \DB\Param("my name"));
+        
+        $q = $Q::select("*")->from(\DB\Identifier::{"MyTable"}())->where(\DB\Identifier::{"Username"}(), "=", new \DB\Param("my name"))->and(\DB\Identifier::{"Name"}(), "LIKE", new \DB\Param("%search name"))->and( $Q::_(\DB\Identifier::{"Name"}())->likeEscape( new \DB\Param("%search name_") ) );
+
+        echo $q;
+        echo "<br>\n";
+        print_r($q->getNamedParams());
     }
 }
